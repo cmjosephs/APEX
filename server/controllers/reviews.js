4192c43d314c6ae8eb9834873875171db0e47ad8
Tiffanie Vu <tiffanyv.330@gmail.com>
2022-02-11 15:08:33 -0800

    Fixed review routes again

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -10,15 +10,29 @@ const options = {
 
 module.exports = {
 
-  getReview: (req, res) => {
-    axios.get(`${baseUrl}/products/${req.params.id}/reviews`, options)
+  getReviewById: (req, res) => {
+    axios.get(`${baseUrl}/products/${req.params.product_id}/reviews`, options)
     .then(results => res.status(200).send(results.data))
     .catch(err => res.status(404).send(err));
   },
-
-  addReview: (req, res) => {
-    axios.post(`${baseUrl}/products/${req.params.id}/reviews`, options)
+  addReviewById: (reviewBody) => {
+    // axios.post(`${baseUrl}/products/${req.params.product_id}/reviews`, options, req.body)
+    // .then(results => res.status(200).send(‘Added review!’))
+    // .catch(err => res.status(404).send(err));
+  },
+  getMetaData: (req, res) => {
+    axios.get(`${baseUrl}/reviews/meta`, options)
     .then(results => res.status(200).send(results.data))
     .catch(err => res.status(404).send(err));
+  },
+  addHelpfulReview: (req, res) => {
+    // axios.put(`${baseUrl}/products/${req.params.product_id}/reviews/${req.params.review_id}/helpful`, options, req.body)
+    // .then(results => res.status(200).send(‘Marked as helpful’))
+    // .catch(err => res.status(404).send(err));
+  },
+  addReportReview: (req, res) => {
+    // axios.put(`${baseUrl}/products/${req.params.product_id}/reviews/${req.params.review_id}/report`, options, req.body)
+    // .then(results => res.status(200).send(‘Reported review’))
+    // .catch(err => res.status(404).send(err));
   }
 }
