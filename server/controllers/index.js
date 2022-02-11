f1e40e30ebc89550c64e73353917c15efe851da4
Chris Josephs <chrismjosephs@gmail.com>
2022-02-11 13:41:26 -0800

    destructure routes require controller dir

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,4 @@
 exports.products = require('./products.js');
-// exports.qA = require('./qA.js');
-// exports.reviews = require('./reviews.js');
-// exports.miscAPI = require('./miscAPI.js');
+exports.qA = require('./qA.js');
+exports.reviews = require('./reviews.js');
+exports.miscAPI = require('./miscAPI.js');
---
+++ server/controllers/miscAPI.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const API_KEY = require('../config/config.js');
+const baseUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax';
+
+const options = {
+  headers: {
+    Authorization: API_KEY
+  }
+}
+
+// edit posts later for AWS cloudfront
+module.exports = {
+  getCart: (req, res) => {
+    axios.get(`${baseUrl}/cart`, options)
+    .then((response) => res.status(200).send(response.data))
+    .catch((err) => res.status(404).send(err))
+  },
+
+  addToCart: (req, res) => {
+    axios.get(`${baseUrl}/cart`, {
+      sku_id: req.body.sku_id,
+      headers: {
+        Authorization: API_KEY
+      }
+    })
+    .then((reponse) => res.sendStatus(201))
+    .catch((err) => res.status(404).send(err));
+  },
+
+  logInteractions: (req, res) => {
+    axios.post(`${baseUrl}/interactions`, options)
+    .then(() => res.sendStatus(201))
+    .catch((err) => res.status(500).send(err))
+  }
+}
