e50087f07f36433fd65af00a3678acd3c8c71b36
Alexandria Norvani <acnorvani@gmail.com>
2022-02-11 14:32:08 -0800

    Added params for product id and commented out post and put due to these will be aws request later

--- server/controllers/qA.js
+++ server/controllers/qA.js
@@ -10,16 +10,16 @@ const options = {
 
 module.exports = {
   getQuestions: (req, res) => {
-    axios.get(`${baseUrl}/qa/questions`, options)
+    axios.get(`${baseUrl}/qa/questions`, {params: {product_id: req.body}, headers: {Authorization: API_KEY}})
     .then((response) => res.status(200).send(response.data))
     .catch((err) => res.status(404).send(err));
   },
 
-  postQuestion: (req, res) => {
-    axios.post(`${baseUrl}/qa/questions`, options, req.body)
-      .then((respose) => res.status(201).send("Question was Posted"))
-      .catch((err) => res.status(400).send(err));
-  },
+  // postQuestion: (req, res) => {
+  //   axios.post(`${baseUrl}/qa/questions`, options, req.body)
+  //     .then((respose) => res.status(201).send("Question was Posted"))
+  //     .catch((err) => res.status(400).send(err));
+  // },
 
   getAnswers: (req, res) => {
     axios.get(`${baseUrl}/qa/questions/${req.params.id}/answers`, options)
@@ -27,33 +27,33 @@ module.exports = {
     .catch((err) => res.status(404)).send(err)
   },
 
-  postAnswer: (req, res) => {
-    axios.post(`${baseUrl}/qa/questions/${req.params.id}/answers`, options, req.body)
-      .then((response) => res.status(201).send("Answer was Posted"))
-      .catch((err) => res.status(404).send(err))
-  },
+  // postAnswer: (req, res) => {
+  //   axios.post(`${baseUrl}/qa/questions/${req.params.id}/answers`, options, req.body)
+  //     .then((response) => res.status(201).send("Answer was Posted"))
+  //     .catch((err) => res.status(404).send(err))
+  // },
 
-  updateQuestionHelpful: (req, res) => {
-    axios.put(`${baseUrl}/qa/questions/${req.params.id}/helpful)`, options, req.body)
-      .then((response) => res.status(200).send(response))
-      .catch((err) => res.status(404).send(err))
-  },
+  // updateQuestionHelpful: (req, res) => {
+  //   axios.put(`${baseUrl}/qa/questions/${req.params.id}/helpful)`, options, req.body)
+  //     .then((response) => res.status(200).send(response))
+  //     .catch((err) => res.status(404).send(err))
+  // },
 
-  updateQuestionReport: (req, res) => {
-    axios.put(`${baseUrl}/qa/questions/${req.params.id}/report)`, options, req.body)
-      .then((response) => res.status(200).send(response))
-      .catch((err) => res.status(404).send(err))
-  },
+  // updateQuestionReport: (req, res) => {
+  //   axios.put(`${baseUrl}/qa/questions/${req.params.id}/report)`, options, req.body)
+  //     .then((response) => res.status(200).send(response))
+  //     .catch((err) => res.status(404).send(err))
+  // },
 
-  updateAnswerHelpful: (req, res) => {
-    axios.put(`${baseUrl}/qa/answers/${req.params.id}/helpful)`, options, req.body)
-      .then((response) => res.status(200).send(response))
-      .catch((err) => res.status(404).send(err))
-  },
+  // updateAnswerHelpful: (req, res) => {
+  //   axios.put(`${baseUrl}/qa/answers/${req.params.id}/helpful)`, options, req.body)
+  //     .then((response) => res.status(200).send(response))
+  //     .catch((err) => res.status(404).send(err))
+  // },
 
-  updateAnswerReport: (req, res) => {
-    axios.put(`${baseUrl}/qa/answers/${req.params.id}/report)`, options, req.body)
-      .then((response) => res.status(200).send(response))
-      .catch((err) => res.status(404).send(err))
-  },
+  // updateAnswerReport: (req, res) => {
+  //   axios.put(`${baseUrl}/qa/answers/${req.params.id}/report)`, options, req.body)
+  //     .then((response) => res.status(200).send(response))
+  //     .catch((err) => res.status(404).send(err))
+  // },
 }
