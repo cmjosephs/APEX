2e8aef9cfe2a77194c99920daf0b5d571420eb73
Alexandria Norvani <acnorvani@gmail.com>
2022-02-11 14:22:56 -0800

    Added axios request for the q-a component

--- server/controllers/qA.js
+++ server/controllers/qA.js
@@ -9,14 +9,51 @@ const options = {
 }
 
 module.exports = {
-  getAllQuestions: (req, res) => {
-    axios.get(`${baseUrl}/products/${req.params.id}`)
+  getQuestions: (req, res) => {
+    axios.get(`${baseUrl}/qa/questions`, options)
     .then((response) => res.status(200).send(response.data))
     .catch((err) => res.status(404).send(err));
   },
 
-  getProduct: (req, res) => {
-    axios.get(`${baseUrl}/products/:product_id`, options)
-    .then
+  postQuestion: (req, res) => {
+    axios.post(`${baseUrl}/qa/questions`, options, req.body)
+      .then((respose) => res.status(201).send("Question was Posted"))
+      .catch((err) => res.status(400).send(err));
+  },
+
+  getAnswers: (req, res) => {
+    axios.get(`${baseUrl}/qa/questions/${req.params.id}/answers`, options)
+    .then((response) => res.status(200).send(response.data))
+    .catch((err) => res.status(404)).send(err)
+  },
+
+  postAnswer: (req, res) => {
+    axios.post(`${baseUrl}/qa/questions/${req.params.id}/answers`, options, req.body)
+      .then((response) => res.status(201).send("Answer was Posted"))
+      .catch((err) => res.status(404).send(err))
+  },
+
+  updateQuestionHelpful: (req, res) => {
+    axios.put(`${baseUrl}/qa/questions/${req.params.id}/helpful)`, options, req.body)
+      .then((response) => res.status(200).send(response))
+      .catch((err) => res.status(404).send(err))
+  },
+
+  updateQuestionReport: (req, res) => {
+    axios.put(`${baseUrl}/qa/questions/${req.params.id}/report)`, options, req.body)
+      .then((response) => res.status(200).send(response))
+      .catch((err) => res.status(404).send(err))
+  },
+
+  updateAnswerHelpful: (req, res) => {
+    axios.put(`${baseUrl}/qa/answers/${req.params.id}/helpful)`, options, req.body)
+      .then((response) => res.status(200).send(response))
+      .catch((err) => res.status(404).send(err))
+  },
+
+  updateAnswerReport: (req, res) => {
+    axios.put(`${baseUrl}/qa/answers/${req.params.id}/report)`, options, req.body)
+      .then((response) => res.status(200).send(response))
+      .catch((err) => res.status(404).send(err))
   },
 }
