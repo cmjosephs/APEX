edc65209151b0c9c3ad3fbf6eca25d4a901fc0aa
Chris Josephs <chrismjosephs@gmail.com>
2022-02-15 18:45:20 -0800

    render available sizes, add state to currently selected sku

--- client/src/Overview/Selectors.jsx
+++ client/src/Overview/Selectors.jsx
@@ -1,8 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { StyleContext } from './Product.jsx';
 
 var Selectors = () => {
   const { allStyles, currentStyle, dispatch } = useContext(StyleContext);
+  // const [stock, setStock] = useState(currentStyle.skus);
+  const [currentSku, setCurrentSku] = useState(null)
+
+  const calcAvailable = () => {
+
+  }
+
+  const handleSelectSize = (sku) => {
+    console.log(sku);
+    setCurrentSku(sku);
+  }
+
 
   const renderStyles = () => {
     return Object.values(allStyles).map((style, index) => {
@@ -19,23 +31,64 @@ var Selectors = () => {
     })
   }
 
-  const renderSizes = () => {
-
+  const renderSizes = (skus) => {
+    let availableSizes = [];
+    for (let sku in skus) {
+      availableSizes.push(
+        <div key={sku}>
+          <input
+            type="radio"
+            id={sku}
+            className="visually-hidden"
+            name="skuAndSize"
+            onClick={(e) => handleSelectSize(e.target.id)}
+          ></input>
+          <label htmlFor={sku}>{skus[sku].size}</label>
+        </div>
+        // <label
+        //   className="skusAndSizes"
+        //   htmlFor={sku}
+        //   key={sku}
+        //   data={skus[sku].quantity}
+        //   onClick={(e) => handleSelectSize()}
+        //   style={{ textDecoration: parseInt(skus[sku].quantity) ? "" : "line-through" }}
+        // >{skus[sku].size}</label>
+      )
+    }
+    return availableSizes;
   }
+
+  // const renderSizes = (skus) => {
+  //   let availableSizes = [];
+  //   for (let sku in skus) {
+  //     availableSizes.push(
+  //       <label
+  //         className="skusAndSizes"
+  //         htmlFor={sku}
+  //         key={sku}
+  //         data={skus[sku].quantity}
+  //         onClick={(e) => handleSelectSize()}
+  //         style={{ textDecoration: parseInt(skus[sku].quantity) ? "" : "line-through" }}
+  //       >{skus[sku].size}</label>
+  //     )
+  //   }
+  //   return availableSizes;
+  // }
 
   if (!Object.keys(currentStyle).length) {
     return <div>Loading</div>
   } else {
     return (
       <div className="selectors">
-        <div className="styles">
+        <div className="style-selector">
           {renderStyles()}
         </div>
-        <div className="sizes">
+        <div className="size-selector">
           size selector
+          {renderSizes(currentStyle.skus)}
         </div>
         <button>Add to Bag</button>
-        <button>Add to your Outfit</button>
+        <button>Favorite</button>
       </div>
     )
   }
