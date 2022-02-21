b7d42e84f3d07b685f035da8d836e677fc40ce16
Chris Josephs <chrismjosephs@gmail.com>
2022-02-20 23:52:06 -0800

    Remove unused code

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect, useLayoutEffect, createContext } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Product from './Overview/Product.jsx';
 import resizeWidth from './Overview/resizeWidth.jsx';
 import ReviewList from './Reviews/ReviewList.jsx';
 import RelatedList from './Related/RelatedList.jsx';
 import QAList from './Questions/QAList.jsx';
-
-import { testProduct, testStyles, testReviewMetaData } from '../../__tests__/OverviewTests/overviewTestData.js'
 
 export const AppContext = createContext();
 
@@ -27,38 +25,26 @@ const App = () => {
       }
     )
     .catch((err) => console.error('Network error: ', err));
-
-
-    // setProductId(parseInt(testReviewMetaData.product_id));
-    // setProductId(42370);
   }
 
   function getProductDetails() {
     axios.get(`/api/products/${productId}`)
     .then(({ data }) => setProductDetails(data))
     .catch(err => console.error(err));
-
-    // setProductDetails(testProduct)
   }
 
   function getProductMetaData() {
     axios.get(`/api/products/${productId}/reviews/meta`)
     .then(({ data }) => setReviewMetaData(data))
     .catch((err) => console.error(err));
-
-    // setReviewMetaData(testReviewMetaData);
   }
 
-  // useEffect(getRandomProductId, []);
   useEffect(() => {
-    // console.log(params.product_id);
-    if (productId) {
       getProductDetails();
       getProductMetaData();
-    }
   }, [productId]);
 
-  if (!productId || !reviewMetaData || !productDetails.id) return <h2>Loading</h2>
+  if (!reviewMetaData || !productDetails.id) return <h2>Loading</h2>
 
   return (
     <AppContext.Provider
@@ -71,11 +57,6 @@ const App = () => {
         <li>Women</li>
         <li>Kids</li>
       </ul> : <ul>lll</ul>}
-        {/* <ul>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
-        </ul> */}
         <h1>{width > 959 ? "APEX" : "A"}</h1>
         <div>
           <input type="text" placeholder="Search" className="nav-search"></input>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,33 +8,10 @@ render(
   // <App />,
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<Navigate to="/products/42368" />}></Route>
+      <Route exact path="/" element={<Navigate to="/products/42370" />}></Route>
       <Route path='/products/:product_id' element={<App />}></Route>
       <Route path='/products/:product_id/:style_id' element={<App />}></Route>
     </Routes>
   </BrowserRouter>,
   document.getElementById('app')
 );
-
-// axios.get('/api/products')
-// .then(
-//   ({ data }) => {
-//     let ids = data.map(product => product.id);
-//     return ids[Math.floor(Math.random()*ids.length)];
-//   }
-// )
-// .then((id) => {
-//   render(
-//     // <App />,
-//     <BrowserRouter>
-//       <Routes>
-//         <Route exact path="/" element={<Navigate to={`/products/${id}`} />}></Route>
-//         {/* <Route exact path="/products" element={<Navigate to="/42368" />}></Route> */}
-//         <Route path='/products/:product_id' element={<App />}></Route>
-//         <Route path='/products/:product_id/:style_id' element={<App />}></Route>
-//       </Routes>
-//     </BrowserRouter>,
-//     document.getElementById('app')
-//   );
-// })
-// .catch((err) => console.error('Network error: ', err));
