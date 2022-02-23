80618a91678a613b3037b758153d225cfdf59771
Chris Josephs <chrismjosephs@gmail.com>
2022-02-22 16:45:31 -0800

    remove MUI rating component, remove calcAvgRating function

--- client/src/Overview/AllDetails.jsx
+++ client/src/Overview/AllDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import Rating from '@mui/material/Rating';
 import AvgRating from '../Shared/AvgRating.jsx';
 import { StyleContext } from './Product.jsx';
 import { AppContext } from '../App.jsx';
@@ -10,27 +9,6 @@ import Info from './Info.jsx';
 var AllDetails = () => {
   const { currentStyle } = useContext(StyleContext);
   const { productId, reviewMetaData, productDetails } = useContext(AppContext);
-  // const [productDetails, setProductDetails] = useState({});
-
-  // function getProductDetails(id) {
-  //   axios.get(`/api/products/${id}`)
-  //   .then(({ data }) => setProductDetails(data))
-  //   .catch(err => consoel.error(err));
-  // }
-
-  function calcAverageRating(obj) {
-    let avgRating = 0;
-    let totalRatings = 0;
-    for (let key in obj) {
-      let quant = parseInt(obj[key]);
-      let rating = parseInt(key);
-      avgRating += quant * rating;
-      totalRatings += quant;
-    }
-    avgRating = avgRating / totalRatings;
-    // return Math.ceil(avgRating / 0.25) * 0.25;
-    return avgRating
-  }
 
   const renderSalePrice = (sale_price, original_price) => {
     if (sale_price) {
@@ -47,7 +25,6 @@ var AllDetails = () => {
     }
   }
 
-
   if (!Object.keys(productDetails).length) {
     return <h3>Loading...</h3>
   } else {
@@ -58,13 +35,6 @@ var AllDetails = () => {
         <h1 className="product-name">{productDetails.name}</h1>
         <h2 className="product-category">{productDetails.category}</h2>
         <div className="avg-rating-title">
-          {/* <Rating
-            name="title-rating-read"
-            value={calcAverageRating(reviewMetaData.ratings)}
-            precision={0.25}
-            sx={{color: "#757575"}}
-            readOnly
-          /> */}
           <AvgRating metaDataRatings={reviewMetaData.ratings} />
           <a href="#review-container" className="same-page-review-link">See all reviews</a>
         </div>
