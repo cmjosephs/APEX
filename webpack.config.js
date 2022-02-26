f70f93be7bbfcb893638717225ee2a1835657ec6
Tiffanie Vu <tiffanyv.330@gmail.com>
2022-02-26 12:03:00 -0800

    Enabled text compression using Brotli

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const SRC_DIR = path.join(__dirname, "/client/src");
 const DIST_DIR = path.join(__dirname, "/client/dist");
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
+var BrotliPlugin = require('brotli-webpack-plugin');
+
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -29,5 +33,31 @@ module.exports = {
         },
       }
     ]
-  }
+  },
+  // plugins: [new CompressionPlugin({
+  //   filename: "[path][base].gz",
+  //   algorithm: "gzip",
+  //   test: /\.js$|\.css$|\.html$/,
+  //   threshold: 840,
+  //   minRatio: 0.8,
+  // })],
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
+    }),
+    new BrotliPlugin({
+      asset: '[file].br',
+      test: /\.(js)$/
+    })
+  ],
 };
